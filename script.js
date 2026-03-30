c607d622841d871f695dd7a8dbfbe00fb3d10010
Nishant wankhade <109646436+nishwan810@users.noreply.github.com>
2026-03-31 01:45:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
-}
-const input = prompt("Enter a string");
+const input = prompt("Enter a string ");
 alert(firstNonRepeatedChar(input)); 
+
+
+function firstNonRepeatedChar(str) {
+	
+for (let i = 0; i < str.length(); i++) {
+	int count =0;
+	for(let j=0; j<str.length(); j++){
+		if(str.charAt(j)==str.charAt(i)){
+			count++;
+		}
+	}
+
+	if(count==1){
+		return str.charAt(i);
+	}
+	
+}
+	return null;
+	
+}
+
